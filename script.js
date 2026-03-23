8f0f2be4d678dbe673bfe537e07c817fce1937c6
tetag606-svg <tetag606@gmail.com>
2026-03-23 09:53:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-mode');
+}
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
